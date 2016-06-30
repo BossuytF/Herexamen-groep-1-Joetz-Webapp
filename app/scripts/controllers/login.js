590059d69bbd18615e78a80ccaaf65a1721e3566
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-06-30 13:17:37 +0200

    bootstrap material design

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the webappApp
+ */
+angular.module('webappApp')
+  .controller('LoginCtrl', function () {
+	
+  });
