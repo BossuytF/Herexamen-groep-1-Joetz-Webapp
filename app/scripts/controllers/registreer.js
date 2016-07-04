2c3895daa22e0b3a1bdcfd610a5a025b253fc01b
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-07-04 18:05:55 +0200

    userservice added

--- app/scripts/controllers/registreer.js
+++ app/scripts/controllers/registreer.js
@@ -9,9 +9,5 @@
  */
 angular.module('webappApp')
   .controller('RegistreerCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
---
+++ app/scripts/services/userservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webappApp.UserService
+ * @description
+ * # UserService
+ * Service in the webappApp.
+ */
+angular.module('webappApp')
+  .service('UserService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
