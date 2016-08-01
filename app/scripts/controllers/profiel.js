3026aa0d2aef5378991c3dc694b1b77e64c73ffd
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-08-02 00:55:53 +0200

    profiel views

--- app/scripts/controllers/profiel.js
+++ app/scripts/controllers/profiel.js
@@ -8,10 +8,38 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('ProfielCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('ProfielCtrl', ['$rootScope', '$mdDialog', '$scope', '$state', function ($rootScope, $mdDialog, $scope, $state) {
+			var profiel = this;
+			profiel.myDate = new Date();
+			profiel.dateToday = new Date(
+					profiel.myDate.getFullYear(),
+					profiel.myDate.getMonth(),
+					profiel.myDate.getDate()-10);
+
+			$scope.$watch('profiel.form', function (form) {
+				if (form) {
+					profiel.form = form;
+				}
+			});
+
+			$rootScope.$on('$stateChangeStart',
+				function (event, toState, toParams, fromState, fromParams, options) {
+				if (profiel.form && !profiel.form.$pristine) {
+					event.preventDefault();
+					$mdDialog.show(confirm).then(function () {
+						profiel.form.$setPristine();
+						$state.go(toState.name)
+					});
+				}
+			});
+
+			var confirm = $mdDialog.confirm()
+				.title('Pas op!')
+				.textContent('Je hebt dingen aangepast die nog niet opgeslaan zijn, wilt u deze aanpasingen wegsmijten')
+				.ariaLabel('Pas op')
+				.targetEvent()
+				.ok('Ja')
+				.cancel('Nee');
+
+		}
+	]);
