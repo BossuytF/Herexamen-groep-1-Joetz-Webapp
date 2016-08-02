b81390b49aabca517cd2156e0b354a71ff6c8c13
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-08-02 14:36:27 +0200

    Kleurrijke poging tot homepage

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,59 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('HomeCtrl', ['blogService', function (blogService) {
+
+			var home = this;
+
+			var COLORS = ['red','green','blue','yellow','purple']
+			
+			home.blogs = [{
+					title : 'TEST1'
+				}, {
+					title : 'TEST2'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}, {
+					title : 'TEST3'
+				}
+			];
+
+			/* 			blogService.getAll().then(function (response) {
+			home.blogs = response.data.blogs
+			}) */
+			giveSpans();
+			function giveSpans() {
+				for (var i = 0; i < home.blogs.length; i++) {
+					home.blogs[i].colspan = Math.floor((Math.random() * 3) + 2);
+					home.blogs[i].rowspan = 2;
+					home.blogs[i].color = randomColor();
+					console.log(home.blogs[i]);
+				}
+			}
+
+			function randomColor() {
+				return COLORS[Math.floor(Math.random() * COLORS.length)];
+			}
+
+			function randomSpan() {
+				var r = Math.random();
+				if (r < 0.8) {
+					return 1;
+				} else if (r < 0.9) {
+					return 2;
+				} else {
+					return 3;
+				}
+			}
+		}
+	]);
---
+++ app/scripts/services/kampenservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name webappApp.kampenService
+ * @description
+ * # kampenService
+ * Service in the webappApp.
+ */
+angular.module('webappApp')
+  .service('kampenService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
