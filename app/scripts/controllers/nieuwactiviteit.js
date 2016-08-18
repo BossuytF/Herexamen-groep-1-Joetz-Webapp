27ebc7c6b70018546077fc870ddbe1fc7c8154c9
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2016-08-18 14:44:57 +0200

    form validatie

--- app/scripts/controllers/nieuwactiviteit.js
+++ app/scripts/controllers/nieuwactiviteit.js
@@ -8,10 +8,23 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('NieuwactiviteitCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('NieuwactiviteitCtrl',['UserService', '$state', '$mdToast', function (UserService, $state, $mdToast) {
+    var nieuwactiviteit = this;
+
+			nieuwactiviteit.activiteit = {
+				titel : '',
+				locatie : '',
+				startdatum : '',
+				einddatum : '',
+				heledag : '',
+			};
+
+			nieuwactiviteit.nieuwactiviteit = function () {
+				UserService.create(nieuwactiviteit.activiteit).then(function () {
+					$mdToast.showSimple("Nieuwe activiteit is met success aangemaakt"); 		
+					$state.go('Home');
+				}, function () {
+					registreer.error = 'Er bestaat al een Activiteit met deze gegevens';
+				});
+			};
   });
