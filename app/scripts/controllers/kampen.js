3998dd71ba0ccef3ef8fe9257176bd4a2da3cfec
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-08-18 12:01:38 +0200

    tussentijdse commit medewerker toewijzen

--- app/scripts/controllers/kampen.js
+++ app/scripts/controllers/kampen.js
@@ -53,7 +53,36 @@ angular.module('webappApp')
 				clickOutsideToClose: false,
                   scope: $scope,        
                   preserveScope: true,           
-                  template: 'vieuws/medewerkers.html',
+                  template:   "<md-content class="md-padding autocomplete" layout="column">
+    <md-contact-chips
+        ng-model="ctrl.contacts"
+        md-contacts="ctrl.querySearch($query)"
+        md-contact-name="name"
+        md-contact-image="image"
+        md-contact-email="email"
+        md-require-match="true"
+        md-highlight-flags="i"
+        filter-selected="ctrl.filterSelected"
+        placeholder="To">
+    </md-contact-chips>
+    <md-list class="fixedRows">
+      <md-subheader class="md-no-sticky">Contacts</md-subheader>
+      <md-list-item class="md-2-line contact-item" ng-repeat="(index, contact) in ctrl.allContacts"
+          ng-if="ctrl.contacts.indexOf(contact) < 0">
+        <img ng-src="{{contact.image}}" class="md-avatar" alt="{{contact.name}}" />
+        <div class="md-list-item-text compact">
+          <h3>{{contact.name}}</h3>
+          <p>{{contact.email}}</p>
+        </div>
+      </md-list-item>
+      <md-list-item class="md-2-line contact-item selected" ng-repeat="(index, contact) in ctrl.contacts">
+        <img ng-src="{{contact.image}}" class="md-avatar" alt="{{contact.name}}" />
+        <div class="md-list-item-text compact">
+          <h3>{{contact.name}}</h3>
+          <p>{{contact.email}}</p>
+        </div>
+      </md-list-item>
+    </md-list>" ,
                   controller: function DialogController($scope, $mdDialog) {
                      $scope.closeDialog = function() {
                         $mdDialog.hide();
