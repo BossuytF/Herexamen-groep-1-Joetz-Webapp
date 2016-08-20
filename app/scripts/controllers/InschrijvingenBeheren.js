d20f9091a5739547ef5e722d690e62254ae55a09
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2016-08-20 17:04:55 +0200

    controller

---
+++ app/scripts/controllers/InschrijvingenBeheren.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappApp.controller:InschrijvenCtrl
+ * @description
+ * # InschrijvenCtrl
+ * Controller of the webappApp
+ */
+angular.module('webappApp')
+  .controller('InschrijvingenBeherenCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
