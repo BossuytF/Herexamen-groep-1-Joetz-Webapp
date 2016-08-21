11c79ee532c4b74e0cc7d5bfeeb71c60e92f95be
Ward Debaene <ward.debaene.t4178@student.hogent.be>
2016-08-21 17:43:50 +0200

    data linking

--- app/scripts/controllers/historiek.js
+++ app/scripts/controllers/historiek.js
@@ -8,10 +8,32 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('HistoriekCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HistoriekCtrl',  ['UserService', '$mdToast', '$mdDialog', '$scope', '$state', function (UserService, $mdToast, $mdDialog, $scope, $state) {
+    var inschrijvingen = this;
+     $scope.test = 'Hello world!';
+
+
+
+			inschrijvingen.InschrijvingsLijst = [];
+
+			function getInschrijvingen() {
+				UserService.get($rootScope.user.email).then(function (response) {
+					profiel.user = response.data;
+					inschrijvingen.InschrijvingsLijst = profiel.user.inschrijvingen;
+				});
+			}
+			getInschrijvingen();
+			if (InschrijvingsLijst.length > 0) {var detailkamp = InschrijvingsLijst[0];};
+			else {
+				var detailkamp ={
+					naam: 'Je hebt nog geen kampen meegedaan',
+					adres.gemeente: '',
+					startDatum: '',
+					eindDatum: '',
+					omschrijving: ''
+			};
+			}
+			$scope.selectKamp = function(inschrijving) {
+			detailkamp = inschrijving.kamp;
+			}
   });
