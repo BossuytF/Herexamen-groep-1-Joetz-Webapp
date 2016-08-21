5e2bb029446be0d745f95cac047e0027eb999b5f
Fabrice Bossuyt <fabrice.bossuyt.s4361@student.hogent.be>
2016-08-21 18:59:21 +0200

    bugfix

--- app/scripts/controllers/historiek.js
+++ app/scripts/controllers/historiek.js
@@ -8,32 +8,37 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('HistoriekCtrl',  ['UserService', '$mdToast', '$mdDialog', '$scope', '$state', function (UserService, $mdToast, $mdDialog, $scope, $state) {
-    var inschrijvingen = this;
-     $scope.test = 'Hello world!';
-
-
+.controller('HistoriekCtrl', ['UserService', '$mdToast', '$mdDialog', '$scope', '$state', '$rootScope', 
+function (UserService, $mdToast, $mdDialog, $scope, $state, $rootScope) {
+			var inschrijvingen = this;
 
 			inschrijvingen.InschrijvingsLijst = [];
 
 			function getInschrijvingen() {
 				UserService.get($rootScope.user.email).then(function (response) {
-					profiel.user = response.data;
-					inschrijvingen.InschrijvingsLijst = profiel.user.inschrijvingen;
+					inschrijvingen.user = response.data;
+					inschrijvingen.InschrijvingsLijst = inschrijvingen.user.inschrijvingen;
 				});
 			}
 			getInschrijvingen();
-			if (InschrijvingsLijst.length > 0) {var detailkamp = InschrijvingsLijst[0];};
-			else {
-				var detailkamp ={
-					naam: 'Je hebt nog geen kampen meegedaan',
-					adres.gemeente: '',
-					startDatum: '',
-					eindDatum: '',
-					omschrijving: ''
-			};
+
+			function setKamp() {
+				if (inschrijvingen.InschrijvingsLijst.length > 0) {
+					inschrijvingen.detailkamp = InschrijvingsLijst[0];
+				} else {
+					inschrijvingen.detailkamp = {
+						naam : 'Je hebt nog geen kampen meegedaan',
+						gemeente : '',
+						startDatum : '',
+						eindDatum : '',
+						omschrijving : ''
+					};
+				}
 			}
-			$scope.selectKamp = function(inschrijving) {
-			detailkamp = inschrijving.kamp;
+			setkamp();
+
+			inschrijvingen.selectKamp = function (inschrijving) {
+				inschrijvingen.detailkamp = inschrijving.kamp;
 			}
-  });
+		}
+	]);
